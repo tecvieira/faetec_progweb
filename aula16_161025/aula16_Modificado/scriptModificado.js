9b2cfc4721fe513e0aa0dc497168e5d799917acf
Robson Vieira <tecvander.vieira@gmail.com>
2025-10-17 22:00:02 -0300

    aula16 modificada
    Agora com intervenção do usuário

--- aula16_161025/aula16_Modificado/scriptModificado.js
+++ aula16_161025/aula16_Modificado/scriptModificado.js
@@ -1,31 +1,31 @@
 function somar(){
     //define os valores fixos dentro da função
-    const n1 = 5;
-    const n2 = 3;
-    const resultado = n1 + n2;
+    var n1 = Number(document.getElementById("campo1").value);
+    var n2 = Number(document.getElementById("campo2").value);
+    var resultado = n1 + n2;
     document.getElementById("alvo").textContent = `Somar: ${resultado}`;
 }
 
 function subtrair(){
     //define os valores fixos dentro da função
-    const n1 = 10;
-    const n2 = 4;
-    const resultado = n1 - n2;
+    var n1 = Number(document.getElementById("campo1").value);
+    var n2 = Number(document.getElementById("campo2").value);
+    var resultado = n1 - n2;
     document.getElementById("alvo").textContent = `Subtração: ${resultado}`;
 }
 
 function multiplicar(){
     //define os valores fixos dentro da função
-    const n1 = 6;
-    const n2 = 7;
+    var n1 = Number(document.getElementById("campo1").value);
+    var n2 = Number(document.getElementById("campo2").value);
     const resultado = n1 * n2;
     document.getElementById("alvo").textContent = `Multiplicação: ${resultado}`;
 }
 
 function dividir(){
     //define os valores fixos dentro da função
-    const n1 = 20;
-    const n2 = 5;
-    const resultado = n1 / n2;
+    var n1 = Number(document.getElementById("campo1").value);
+    var n2 = Number(document.getElementById("campo2").value);
+    var resultado = n1 / n2;
     document.getElementById("alvo").textContent = `Divisão: ${resultado}`;
 }
