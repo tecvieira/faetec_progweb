4fa68986d36ebf7cb9fcfbf662db47fba1b3f5eb
Robson Vieira <tecvander.vieira@gmail.com>
2025-10-23 20:44:11 -0300

    aula 18 incompleto

---
+++ aula18_online/script.js
@@ -0,0 +1,4 @@
+ //Array inicial
+let frutas = ["maçã", "banana","laranja","uva","abacaxi"];
+//mostra o array frutas.join(" , ") em forma de texto
+document.getElementById("arrayBase").textContent = frutas.join(" ,")
