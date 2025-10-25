c3800925657579fc9860f54250ef976a9eb3c6c1
Robson Vieira <tecvander.vieira@gmail.com>
2025-10-24 21:01:37 -0300

    nao funciona java script

--- aula18_online/script.js
+++ aula18_online/script.js
@@ -1,4 +1,17 @@
  //Array inicial
 let frutas = ["maçã", "banana","laranja","uva","abacaxi"];
-//mostra o array frutas.join(" , ") em forma de texto
+
 document.getElementById("arrayBase").textContent = frutas.join(" ,")
+
+function mostrarArray(){
+    exibir(Array atual:[${frutas.join(",")}]);
+}
+
+
+
+
+//Adiciona um ítem ao final do array
+function adicionarItem(){
+    frutas.push("manga");
+    exibir("Adicionado: manga <br> Array agora:" + frutas.join(" ,"));
+}
